0517adcd9d965a226b8b8f76d2cd02100904c7c7
alex <awkinley@metrika.co>
2022-01-15 09:52:04 -0500

    Basic Server startup settings

--- src/main/java/doom/DoomServer.java
+++ src/main/java/doom/DoomServer.java
@@ -2,13 +2,59 @@ package doom;
 
 import doom.player.DoomPlayer;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.*;
+import net.minestom.server.instance.batch.ChunkBatch;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.world.biomes.Biome;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class DoomServer {
     public static void main(String[] args) {
         MinecraftServer server = MinecraftServer.init();
-
         MinecraftServer.getConnectionManager().setPlayerProvider(DoomPlayer::new);
+
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setChunkGenerator(new GeneratorDemo());
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
 
         server.start("0.0.0.0", 25565);
     }
+    private static class GeneratorDemo implements ChunkGenerator {
+
+        @Override
+        public void generateChunkData(@NotNull ChunkBatch batch, int chunkX, int chunkZ) {
+            // Set chunk blocks
+            for (byte x = 0; x < Chunk.CHUNK_SIZE_X; x++) {
+                for (byte z = 0; z < Chunk.CHUNK_SIZE_Z; z++) {
+                    for (byte y = 0; y < 40; y++) {
+                        batch.setBlock(x, y, z, Block.STONE);
+                    }
+                }
+            }
+        }
+
+//        @Override
+//        public void fillBiomes(Biome[] biomes, int chunkX, int chunkZ) {
+//            Arrays.fill(biomes, Biome.PLAINS);
+//        }
+
+        @Override
+        public List<ChunkPopulator> getPopulators() {
+            return null;
+        }
+    }
 }
+
